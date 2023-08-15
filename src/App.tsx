e2031d3ba34293bc4058a9ae2ab5dacc715950b9
Olga Prykhodko <helga.prikhodko@gmail.com>
2023-08-15 11:44:52 +0300

    ⭐ feat(App:) add  AppLayout with Header and Sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
 import Account from "@/pages/Account";
+import AppLayout from "./ui/AppLayout";
 import Bookings from "@/pages/Bookings";
 import Cabins from "@/pages/Cabins";
 import Dashboard from "@/pages/Dashboard";
@@ -10,21 +11,22 @@ import Settings from "@/pages/Settings";
 import Users from "@/pages/Users";
 import GlobalStyles from "./styles/globalStyles";
 
-interface AppProps {}
-
-const App: React.FC<AppProps> = ({}) => {
+const App = () => {
   return (
     <>
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
-          <Route index element={<Navigate replace to="dashboard" />} />
-          <Route path="dashboard" element={<Dashboard />} />
-          <Route path="bookings" element={<Bookings />} />
-          <Route path="cabins" element={<Cabins />} />
-          <Route path="users" element={<Users />} />
-          <Route path="settings" element={<Settings />} />
-          <Route path="acount" element={<Account />} />
+          <Route element={<AppLayout />}>
+            <Route index element={<Navigate replace to="dashboard" />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="bookings" element={<Bookings />} />
+            <Route path="cabins" element={<Cabins />} />
+            <Route path="users" element={<Users />} />
+            <Route path="settings" element={<Settings />} />
+            <Route path="account" element={<Account />} />
+          </Route>
+
           <Route path="login" element={<Login />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
