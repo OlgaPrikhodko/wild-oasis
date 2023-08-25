a6bd7ff1166debdb59e34550aeedb698b9423021
Olga Prykhodko <helga.prikhodko@gmail.com>
2023-08-25 16:26:55 +0300

    :star: feat(SettingsForm): update settings

--- src/features/settings/UpdateSettingsForm.tsx
+++ src/features/settings/UpdateSettingsForm.tsx
@@ -4,6 +4,8 @@ import Input from "@/ui/form/Input";
 import Spinner from "@/ui/Spinner";
 
 import { useSettings } from "./useSettings";
+import { useUpdateSetting } from "./useUpdateSetting";
+import { SettingsType } from "@/types/supabase.types";
 
 function UpdateSettingsForm() {
   const {
@@ -16,32 +18,59 @@ function UpdateSettingsForm() {
     } = {},
   } = useSettings();
 
-  function handleSubmit(data: unknown) {
-    console.log(data);
-  }
+  const { isUpdating, updateSetting } = useUpdateSetting();
 
   if (isLoading) return <Spinner />;
 
+  function handleUpdate(
+    e: React.FocusEvent<HTMLInputElement, Element>,
+    field: keyof SettingsType
+  ) {
+    const { value } = e.target;
+    if (!value) return;
+
+    updateSetting({ [field]: Number(value) });
+  }
+
   return (
-    <Form onSubmit={handleSubmit}>
+    <Form>
       <FormRow label="Minimum nights/booking">
-        <Input type="number" id="min-nights" defaultValue={minBookingLength} />
+        <Input
+          type="number"
+          id="min-nights"
+          disabled={isUpdating}
+          defaultValue={minBookingLength}
+          onBlur={(e) => handleUpdate(e, "minBookingLength")}
+        />
       </FormRow>
+
       <FormRow label="Maximum nights/booking">
-        <Input type="number" id="max-nights" defaultValue={maxBookingLength} />
+        <Input
+          type="number"
+          id="max-nights"
+          disabled={isUpdating}
+          defaultValue={maxBookingLength}
+          onBlur={(e) => handleUpdate(e, "maxBookingLength")}
+        />
       </FormRow>
+
       <FormRow label="Maximum guests/booking">
         <Input
           type="number"
           id="max-guests"
+          disabled={isUpdating}
           defaultValue={maxGuestsPerBooking}
+          onBlur={(e) => handleUpdate(e, "maxGuestsPerBooking")}
         />
       </FormRow>
+
       <FormRow label="Breakfast price">
         <Input
           type="number"
           id="breakfast-price"
+          disabled={isUpdating}
           defaultValue={breakfastPrice}
+          onBlur={(e) => handleUpdate(e, "breakfastPrice")}
         />
       </FormRow>
     </Form>
--- src/services/apiSettings.ts
+++ src/services/apiSettings.ts
@@ -1,6 +1,5 @@
-import supabase from "./supabase";
-
 import { SettingsType } from "@/types/supabase.types";
+import supabase from "./supabase";
 
 export async function getSettings() {
   const { data, error } = await supabase.from("settings").select("*").single();
@@ -10,4 +9,21 @@ export async function getSettings() {
     throw new Error("Settings could not be loaded");
   }
   return data as SettingsType;
+}
+
+type UpdateSettingsType = Partial<SettingsType>;
+// We expect a newSetting object that looks like {setting: newValue}
+export async function updateSetting(newSetting: UpdateSettingsType) {
+  const { data, error } = await supabase
+    .from("settings")
+    .update(newSetting)
+    // There is only ONE row of settings, and it has the ID=1, and so this is the updated one
+    .eq("id", 1)
+    .single();
+
+  if (error) {
+    console.error(error);
+    throw new Error("Settings could not be updated");
+  }
+  return data;
 }
