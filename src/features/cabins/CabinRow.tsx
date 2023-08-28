7cfb6db3f1688b827e5e0b4e512bf8ee06336858
Olga Prykhodko <helga.prikhodko@gmail.com>
2023-08-28 11:51:33 +0300

    :star: feat(Table): add reusable Table

--- src/features/cabins/CabinRow.tsx
+++ src/features/cabins/CabinRow.tsx
@@ -4,23 +4,12 @@ import { HiPencil, HiSquare2Stack, HiTrash } from "react-icons/hi2";
 import CreateCabinForm from "./CreateCabinForm";
 import ConfirmDelete from "@/ui/ConfirmDelete";
 import Modal from "@/ui/Modal";
+import Table from "@/ui/Table";
 
 import { useDeleteCabin } from "./useDeleteCabin";
+import { useCreateCabin } from "./useCreateCabin";
 import { CabinType } from "@/types/supabase.types";
 import { formatCurrency } from "@/utils/helpers";
-import { useCreateCabin } from "./useCreateCabin";
-
-const TableRow = styled.div`
-  display: grid;
-  grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
-  column-gap: 2.4rem;
-  align-items: center;
-  padding: 1.4rem 2.4rem;
-
-  &:not(:last-child) {
-    border-bottom: 1px solid var(--color-grey-100);
-  }
-`;
 
 const Img = styled.img`
   display: block;
@@ -74,7 +63,7 @@ const CabinRow = ({ cabin }: { cabin: CabinType }) => {
   }
 
   return (
-    <TableRow>
+    <Table.Row>
       <Img src={image} />
       <Cabin>{name}</Cabin>
       <div>Fits up to {maxCapacity} guests</div>
@@ -113,7 +102,7 @@ const CabinRow = ({ cabin }: { cabin: CabinType }) => {
           </Modal.Window>
         </Modal>
       </div>
-    </TableRow>
+    </Table.Row>
   );
 };
 
