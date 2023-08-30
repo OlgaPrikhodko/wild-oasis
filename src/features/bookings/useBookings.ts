516348b87b425280a75981247c023ec9eeb73f20
Olga Prykhodko <helga.prikhodko@gmail.com>
2023-08-30 11:25:36 +0300

    :star: feat(Bookings): add API-side filtering

--- src/features/bookings/useBookings.ts
+++ src/features/bookings/useBookings.ts
@@ -1,10 +1,20 @@
 import { useQuery } from "@tanstack/react-query";
-import { getBookings } from "@/services/apiBooking";
+import { useSearchParams } from "react-router-dom";
+import { FilterType, getBookings } from "@/services/apiBooking";
 
 export function useBookings() {
+  const [searchParams] = useSearchParams();
+
+  //FILTER
+  const filterValue = searchParams.get("status");
+  const filter: FilterType =
+    !filterValue || filterValue === "all"
+      ? null
+      : { field: "status", value: filterValue, method: "eq" };
+
   const { isLoading, data, error, isSuccess } = useQuery({
-    queryKey: ["bookings"],
-    queryFn: getBookings,
+    queryKey: ["bookings", filter],
+    queryFn: () => getBookings({ filter }),
   });
 
   return { isLoading, bookings: isSuccess ? data : [], error };
--- src/services/apiBooking.ts
+++ src/services/apiBooking.ts
@@ -1,12 +1,30 @@
 import supabase from "./supabase";
 import { BookingRowType } from "@/types/supabase.types";
 
-export async function getBookings() {
-  const { data, error } = await supabase
+// fields "status"
+export type FilterMethodType = "eq" | "lte" | "gte";
+export type FilterType = {
+  field: string;
+  value: string;
+  method?: FilterMethodType;
+} | null;
+type SortType = { field: string; value: string };
+type GetBookingsProps = { filter: FilterType; sortBy?: SortType };
+
+export async function getBookings({
+  filter,
+  sortBy,
+}: GetBookingsProps): Promise<BookingRowType[]> {
+  let query = supabase
     .from("bookings")
     .select(
       "id, created_at, startDate, endDate, numNights, numGuests, status, totalPrice, cabins(name), guests(fullName, email)"
     );
+
+  // FILTER
+  if (filter !== null)
+    query = query[filter.method || "eq"](filter.field, filter.value);
+  const { data, error } = await query;
 
   if (error) {
     console.log(error);
