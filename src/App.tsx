dbc6b5e068331ca6d5f78c8c8178f20f8123004d
Olga Prykhodko <helga.prikhodko@gmail.com>
2023-08-31 10:21:22 +0300

    :star: feat(App): add booking detail page

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,21 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Toaster } from "react-hot-toast";
+import { StyleSheetManager } from "styled-components";
+import isPropValid from "@emotion/is-prop-valid";
 
 import Account from "@/pages/Account";
 import AppLayout from "./ui/AppLayout";
+import Booking from "./pages/Booking";
 import Bookings from "@/pages/Bookings";
 import Cabins from "@/pages/Cabins";
 import Dashboard from "@/pages/Dashboard";
+import GlobalStyles from "./styles/globalStyles";
 import Login from "@/pages/Login";
 import PageNotFound from "@/pages/PageNotFound";
 import Settings from "@/pages/Settings";
 import Users from "@/pages/Users";
-import GlobalStyles from "./styles/globalStyles";
-import { Toaster } from "react-hot-toast";
-import { StyleSheetManager } from "styled-components";
-import isPropValid from "@emotion/is-prop-valid";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -35,6 +36,7 @@ const App = () => {
             <Route element={<AppLayout />}>
               <Route index element={<Navigate replace to="dashboard" />} />
               <Route path="dashboard" element={<Dashboard />} />
+              <Route path="bookings/:bookingId" element={<Booking />} />
               <Route path="bookings" element={<Bookings />} />
               <Route path="cabins" element={<Cabins />} />
               <Route path="users" element={<Users />} />
---
+++ src/pages/Booking.tsx
@@ -0,0 +1,4 @@
+function Booking() {
+  return <div>Booking details</div>;
+}
+export default Booking;
