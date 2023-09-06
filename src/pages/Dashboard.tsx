91bb887e5b3b7a055255050c86587f738d5b3439
Olga Prykhodko <helga.prikhodko@gmail.com>
2023-09-06 15:44:47 +0300

    :star: feat(Dashboard): add DashboardLayout

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,14 +1,17 @@
+import DashboardLayout from "@/features/dashboard/DashboardLayout";
 import Heading from "@/ui/Heading";
 import Row from "@/ui/Row";
 
-interface DashboardProps {}
-
-const Dashboard: React.FC<DashboardProps> = ({}) => {
+const Dashboard = () => {
   return (
-    <Row type="horizontal">
-      <Heading as="h1">Dashboard</Heading>
-      <p>TEST</p>
-    </Row>
+    <>
+      <Row type="horizontal">
+        <Heading as="h1">Dashboard</Heading>
+        <p>TEST</p>
+      </Row>
+
+      <DashboardLayout />
+    </>
   );
 };
 
