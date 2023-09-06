510ee872a6387e6878abff590087000d35edadfd
Olga Prykhodko <helga.prikhodko@gmail.com>
2023-09-06 15:07:10 +0300

    :star: feat(darkMode): add class to the root depending on the mode

--- src/context/DarkModeContext.tsx
+++ src/context/DarkModeContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useEffect } from "react";
 import { useLocalStorageState } from "@/hooks/useLocalStorageState";
 
 type DarkModeContextType = {
@@ -15,6 +15,16 @@ function DarkModeProvider({ children }: { children: React.ReactNode }) {
     false,
     "isDarkMode"
   );
+
+  useEffect(function () {
+    if (isDarkMode) {
+      document.documentElement.classList.add("dark-mode");
+      document.documentElement.classList.remove("light-mode");
+    } else {
+      document.documentElement.classList.add("light-mode");
+      document.documentElement.classList.remove("dark-mode");
+    }
+  });
 
   function toggleDarkMode() {
     setIsDarkMode((isDark: boolean) => !isDark);
