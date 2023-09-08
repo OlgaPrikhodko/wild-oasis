8123db2fab89f7ae5480b185655e6400c39ac733
Olga Prykhodko <helga.prikhodko@gmail.com>
2023-09-08 15:56:08 +0300

    :star: feat(Dashboard): display stays for the current day

--- src/features/dashboard/DashboardLayout.tsx
+++ src/features/dashboard/DashboardLayout.tsx
@@ -4,6 +4,7 @@ import DurationChart from "./DurationChart";
 import SalesChart from "./SalesChart";
 import Spinner from "@/ui/Spinner";
 import Stats from "./Stats";
+import TodayActivity from "../check-in-out/TodayActivity";
 
 import { useCabins } from "../cabins/useCabins";
 import { useRecentBookings } from "./useRecentBookings";
@@ -37,7 +38,7 @@ function DashboardLayout() {
         cabinCount={cabins.length}
       />
 
-      <div>Today's activity</div>
+      <TodayActivity />
       <DurationChart confirmedStays={confirmedStays} />
       <SalesChart bookings={bookings} numDays={numDays} />
     </StyledDashboardLayout>
